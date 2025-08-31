a2c20d92e6df259554187afe8299f4ff0e076032
Namancj9 <115418905+Namancj9@users.noreply.github.com>
2025-08-31 17:08:13 +0530

    Experiment 1.3.c

---
+++ Experiment 1/Experiment 1.3.c
@@ -0,0 +1,13 @@
+/*3. Write a C program to add two numbers, take number from user.*/
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+    printf("Enter first number:");
+    scanf("%d",&a);
+    printf("Enter second number:");
+    scanf("%d",&b);
+    printf("\nsum:%d",a + b);
+    return 0;
+}
