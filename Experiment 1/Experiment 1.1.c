79e8fa754c96a74f07ff6c17b2c06c7268883970
Namancj9 <115418905+Namancj9@users.noreply.github.com>
2025-08-31 17:06:07 +0530

    Create Experiment 1.1.c

---
+++ Experiment 1/Experiment 1.1.c
@@ -0,0 +1,11 @@
+ /*Experiment 1: Installation, Environment Setup and starting with C language
+1. Write a C program to print “ Hello World”
+*/
+#include <stdio.h>
+
+int main() {
+    printf("Name - Naman Kataria\nSAP ID - 590025455\nCourse - BSC CS");
+    printf("\n---------------------------------\n");
+    printf("Hello World");
+
+    return 0;
