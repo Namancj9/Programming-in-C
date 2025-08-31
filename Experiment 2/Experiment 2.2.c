11ca4fba13e697742f988745a16feac3de6ff008
Namancj9 <115418905+Namancj9@users.noreply.github.com>
2025-08-31 17:10:22 +0530

    Create Experiment 2.2.c

---
+++ Experiment 2/Experiment 2.2.c
@@ -0,0 +1,10 @@
+/*WAP a C program to Convert temperature from Celsius to Fahrenheit using the
+formula: F = (C * 9/5) + 32. */
+#include <stdio.h>
+int main(){
+    float f, c;
+    printf("Enter temperature in celcius:");
+    scanf("%f",&c);
+    f = (c * 9/5 + 32);
+    printf("Temperature in fahrenheit:%.2f",f);
+}
