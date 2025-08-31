bf585df2a338f05d069ccd9743d84edda4e6ca69
Namancj9 <115418905+Namancj9@users.noreply.github.com>
2025-08-31 17:07:09 +0530

    Create Experiment 1.2.c

---
+++ Experiment 1/Experiment 1.2.c
@@ -0,0 +1,12 @@
+/*2. Write a C Program to print the address in multiple lines (new line). 
+*/
+#include <stdio.h>
+
+int main() {
+    printf("Hello World\n");
+    printf("New York, USA\n");
+    printf("ZIP Code: 136118\n");
+    printf("-------------------------\n");
+
+    return 0;
+}
