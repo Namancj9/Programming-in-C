41792ba8f830a6d84f7a62386dd64e1858659c3d
Namancj9 <115418905+Namancj9@users.noreply.github.com>
2025-10-27 21:22:55 +0530

    Add program to print diamond pattern of numbers

---
+++ Experiment 4/Experiment 4.3.8.c
@@ -0,0 +1,40 @@
+// Experiment 4.3.8
+#include <stdio.h>
+
+int main() {
+    int rows = 5;
+
+    for (int i = 1; i <= rows; i++) {
+        for (int k = 1; k <= rows - i; k++) {
+            printf(" ");
+        }
+
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        for (int j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    for (int i = rows - 1; i >= 1; i--) {
+        for (int k = 1; k <= rows - i; k++) {
+            printf(" ");
+        }
+
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        for (int j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
