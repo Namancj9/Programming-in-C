bfb05ebbee178ecdc9d6135f5a04c142a414384e
Namancj9 <115418905+Namancj9@users.noreply.github.com>
2025-10-27 21:19:24 +0530

    Add C program for printing number pyramid

---
+++ Experiment 4/Experiment 4.3.5.c
@@ -0,0 +1,20 @@
+// Experiment 4.3.5
+#include <stdio.h>
+
+int main() {
+
+    int rows = 5;
+
+    for (int i = 1; i <= rows; i++){
+        
+        for (int k = 1; k <= rows - i; k++){
+            printf("  "); 
+        }
+
+        for (int j = 1; j <= i; j++){
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
