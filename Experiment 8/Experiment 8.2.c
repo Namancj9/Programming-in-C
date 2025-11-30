3b07c0100565163a5b87d0e411e7a870471a5ff3
Namancj9 <115418905+Namancj9@users.noreply.github.com>
2025-12-01 05:20:39 +0530

    Add Experiment 8.2.c for pointer manipulation

---
+++ Experiment 8/Experiment 8.2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+int i = 42;
+float f = 3.14;
+char c = 'A';
+
+int *ptr_i = &i;
+float *ptr_f = &f;
+char *ptr_c = &c;
+
+printf("Original addresses:\n");
+printf("Integer pointer: %p, Value: %d\n", (void*)ptr_i, *ptr_i);
+printf("Float pointer  : %p, Value: %.2f\n", (void*)ptr_f, *ptr_f);
+printf("Char pointer   : %p, Value: %c\n\n", (void*)ptr_c, *ptr_c);
+
+ptr_i++;
+ptr_f++;
+ptr_c++;
+
+printf("After increment:\n");
+printf("Integer pointer: %p, Value: %d (may be garbage)\n", (void*)ptr_i, *ptr_i);
+printf("Float pointer  : %p, Value: %.2f (may be garbage)\n", (void*)ptr_f, *ptr_f);
+printf("Char pointer   : %p, Value: %c (may be garbage)\n\n", (void*)ptr_c, *ptr_c);
+
+ptr_i--;
+ptr_f--;
+ptr_c--;
+
+printf("After decrement (back to original):\n");
+printf("Integer pointer: %p, Value: %d\n", (void*)ptr_i, *ptr_i);
+printf("Float pointer  : %p, Value: %.2f\n", (void*)ptr_f, *ptr_f);
+printf("Char pointer   : %p, Value: %c\n", (void*)ptr_c, *ptr_c);
+
+return 0;
+
+}
