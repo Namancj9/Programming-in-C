ba3771f2067ec3e1184b978f9c3710117c56f4f2
Namancj9 <115418905+Namancj9@users.noreply.github.com>
2025-12-01 05:14:41 +0530

    Add union Address and main function for address handling

---
+++ Experiment 7/Experiment 7.3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+union Address {
+char name[50];
+char home_address[100];
+char hostel_address[100];
+char city[50];
+char state[50];
+char zip[20];
+};
+
+int main() {
+union Address addr;
+
+strcpy(addr.hostel_address, "Room 101, Block B, ABC Hostel");
+
+printf("Present Address: %s\n", addr.hostel_address);
+
+return 0;
+
+}
