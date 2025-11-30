bde4bb1aa2ebe2fb4a839ad3ffb319cd631490ab
Namancj9 <115418905+Namancj9@users.noreply.github.com>
2025-12-01 05:17:23 +0530

    Add Experiment 8.1.c for pointer demonstration

---
+++ Experiment 8/Experiment 8.1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+int i = 42;
+float f = 3.14;
+char c = 'A';
+
+int *ptr_i = &i;
+float *ptr_f = &f;
+char *ptr_c = &c;
+
+printf("Integer value: %d, Pointer address: %p, Value via pointer: %d\n", i, (void*)ptr_i, *ptr_i);
+printf("Float value  : %.2f, Pointer address: %p, Value via pointer: %.2f\n", f, (void*)ptr_f, *ptr_f);
+printf("Char value   : %c, Pointer address: %p, Value via pointer: %c\n", c, (void*)ptr_c, *ptr_c);
+
+return 0;
+
+}
